aaeee6afaac10e89bc2d740d14336e76586fcbbb
debbie <drosenfeld87@gmail.com>
2018-03-16 14:17:43 -0400

    books controller still works with respond to block added:

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,19 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
-  end
 
+    respond_to do |format|
+      format.html
+      format.text
+      format.json do
+        render json: @books.map { |book|
+          { title: book.title,
+            author: book.author,
+            already_read: book.already_read
+          }
+        }
+      end
+    end
+
+  end
 end
